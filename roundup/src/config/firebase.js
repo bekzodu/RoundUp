d40e443a5972c33e62f42c347e628219925abcf4
begzod usmanov <begzod0320@gmail.com>
2024-11-16 14:59:35 -0600

    fixed leaked API key

--- roundup/src/config/firebase.js
+++ roundup/src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBn3Gax7Fz4VuiLskApL_CFI1hWPh_YBgA",
-  authDomain: "roundup-6684d.firebaseapp.com",
-  projectId: "roundup-6684d",
-  storageBucket: "roundup-6684d.firebasestorage.app",
-  messagingSenderId: "516736422205",
-  appId: "1:516736422205:web:edbdaa5d80066f5d5bf554",
-  measurementId: "G-WD9D9NN8T0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
